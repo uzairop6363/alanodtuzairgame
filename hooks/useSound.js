22ab531ba95341fafa958395aeb75e9465dd1398
uzairop6363 <uzairop6363@gmail.com>
2025-09-16 07:57:35 +0500

    Update useSound.js

--- hooks/useSound.js
+++ hooks/useSound.js
@@ -1,30 +1,5 @@
-import { useEffect, useRef } from 'react';
-
-export function useBackgroundMusic() {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    if (!ref.current) {
-      ref.current = new Audio('/sounds/bg.mp3');
-      ref.current.loop = true;
-      ref.current.volume = 0.4;
-    }
-    const audio = ref.current;
-
-    function startMusic() {
-      audio.play().catch(() => {});
-      window.removeEventListener('click', startMusic);
-    }
-    window.addEventListener('click', startMusic);
-
-    return () => {
-      audio.pause();
-    };
-  }, []);
-}
-
-export function playSound(file, volume=0.7) {
-  const a = new Audio(`/sounds/${file}`);
-  a.volume = volume;
-  a.play();
+// Sounds disabled version
+export function playSound() {
+  // No sound will play, safe for deploy
+  return;
 }
